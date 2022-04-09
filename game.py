3905da9d0d5da9cc16ee55210231fff876c277a4
Mariia <m_shevchenko@fanshaweonline.ca>
2022-04-09 14:56:24 -0400

    Updating new variables

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
 from random import randint
 
 while player_choice is False:
-	print("██████► ◄██████ Marvel YES or No Game ██████► ◄██████")
+	print("██████► ◄██████ Marvel YES or NO Game ██████► ◄██████")
 	print(" Think of one of the Marvel’s characters in the list and answer the question Y or N. At the end computer should give your character.")
 
-	player_choice = input ("Woul you like to start a game?")
+	player_choice = input ("Would you like to start a game?")
 
 
 
