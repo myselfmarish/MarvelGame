e1919049e20e21ca898fe3d796d89ac05ed25af3
Mariia <m_shevchenko@fanshaweonline.ca>
2022-04-09 14:44:54 -0400

    Updating the beginning of the game

---
+++ game.py
@@ -0,0 +1,12 @@
+from random import randint
+
+while player_choice is False:
+	print("██████► ◄██████ Marvel YES or No Game ██████► ◄██████")
+	print(" Think of one of the Marvel’s characters in the list and answer the question Y or N. At the end computer should give your character.")
+
+	player_choice = input ("Woul you like to start a game?")
+
+
+
+
+
